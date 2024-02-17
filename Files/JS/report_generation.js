14af7f3883a7869db9dce97db6dc2ce84a2d0c98
RUDRA-PATEL-1969 <126533592+RUDRA-PATEL-1969@users.noreply.github.com>
2024-02-17 15:20:18 -0800

    Report Generation Update

--- Files/JS/report_generation.js
+++ Files/JS/report_generation.js
@@ -1,10 +1,1 @@
-import * as JsonImport from "../Business-Partners.json";
-
-// Uses a promise to import the json file and then access its data using dot notation
-JsonImport.then(JSON_Data => {
-    // Access the imported data using dot notation
-    console.log(JSON_Data)
-}).catch(error => {
-    throw new Error('Error importing business partner data. Please try again later.');
-    // Do not console log the error as it may reveal potentially sensitive info
-});
+const fs_req = require("fs");
